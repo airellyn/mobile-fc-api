59f2d8e779307b1897ee04ea2ea921c58e0a0509
nauffarms <nauffarms@gmail.com>
2023-03-20 17:26:26 +0700

    update mobile api

--- app/Http/Controllers/API/NasabahController.php
+++ app/Http/Controllers/API/NasabahController.php
@@ -1,7 +1,6 @@
-<?php
-namespace App\Http\Controllers\API;
+<?php 
 
-use App\Http\Controllers\Controller;
+namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
@@ -42,7 +41,6 @@ class NasabahController extends Controller
 
             $sisa_tagihan = 0;
             $total_amcoll = 0;
-
             if (empty($sum_amcoll->total_amcoll) || $sum_amcoll->total_amcoll == 0){
                 $sisa_tagihan = $nasabah->TOTAL_TAGIHAN - $json->amcoll;
                 $total_amcoll = $json->amcoll;
@@ -50,16 +48,13 @@ class NasabahController extends Controller
                 $total_amcoll = $json->amcoll + $sum_amcoll->total_amcoll;
                 $sisa_tagihan = $nasabah->TOTAL_TAGIHAN - $total_amcoll;
             }
-
-
-            //Start FOTO
             if(isset($json->foto_kwitansi)) {
                 $foto_kwitansi = $json->foto_kwitansi;
             }else{
                 $foto_kwitansi = '';
             }
 
-            if(isset($json->foto_jalan)) {
+	    if(isset($json->foto_jalan)) {
                 $foto_jalan = $json->foto_jalan;
             }else{
                 $foto_jalan = '';
@@ -70,32 +65,18 @@ class NasabahController extends Controller
             }else{
                 $foto = '';
             }
-            // End FOTO
-
-            // STATUS VISIT 1 = BAYAR                           STATUS BAYAR 1 = BAYAR FULL
-            // STATUS VISIT 2 = Janji Bayar / PTP               STATUS BAYAR 2 = PTP
-            // STATUS VISIT 3 = Titip Pesan                     STATUS BAYAR 3 = BAYAR SEBAGIAN
-            // STATUS VISIT 4 = Menolak Membayar
-            // STATUS VISIT 5 = Meninggal Dunia
-            // STATUS VISIT 6 =  Dugaan Penipuan
-            // STATUS VISIT 7 = Meninggal Dunia
             
 			if ($json->status_visit == '') {
-				$res = ['status' => false, 'message' => 'Status tidak boleh kosong!!'];
+				$res = ['status' => false, 'message' => 'Status tidak boleh kosong!'];
 	            return response($res,422);
             }
 
-
-            //////////////// PTP //////////////
-
-            //janji bayar atau status bayar PTP
             if (($json->status_visit == 2)||($json->status_bayar == 2)) {
                 if($json->tanggal_ptp == ''){
                     $res = ['status' => false, 'message' => 'Tanggal PTP tidak boleh kosong!'];
                     return response($res,422);
                 }
             }
-            //janji bayar dan status bayar bukan PTP
             if(($json->status_visit == 2)&&($json->status_bayar != 2)) {
                 $res = ['status' => false, 'message' => 'Metode Pembayaran harus diisi Janji Bayar/PTP'];
                 return response($res,422);
@@ -105,26 +86,16 @@ class NasabahController extends Controller
                 return response($res,422);
             }
 
-            //////////////// BAYAR //////////////
-
-            //status visit bayar dan status bayar PTP
-            // if(($json->foto_kwitansi != '')&&($json->status_bayar == '')) {
-            //     $res = ['status' => false, 'message' => 'Status kunjungan harap di isi Bayar'];
-            //     return response($res,422);
-            // }
-
             if(($json->status_visit == 1)&&($json->status_bayar == 2)) {
                 $res = ['status' => false, 'message' => 'Metode Pembayaran harus diisi Bayar Full/Bayar Sebagian'];
                 return response($res,422);
             }
-            
-            //status visit bayar dan status amcol kosong
             if(($json->status_visit == 1)&&($json->amcoll == '')) {
                 $res = ['status' => false, 'message' => 'Amcoll tidak boleh kosong!'];
                 return response($res,422);
             }
-
-            if(($json->status_visit == 7)&&($json->foto_jalan == '')) {
+		
+		if(($json->status_visit == 7)&&($json->foto_jalan == '')) {
                 $res = ['status' => false, 'message' => 'Foto Rumah atau Jalan tidak boleh kosong!'];
                 return response($res,422);
             }
@@ -145,7 +116,7 @@ class NasabahController extends Controller
                 return response($res,422);
             }
 
-            if(($json->status_visit == 7)&&($json->foto == '')) {
+	if(($json->status_visit == 7)&&($json->foto == '')) {
                 $res = ['status' => false, 'message' => 'Foto Rumah atau Jalan tidak boleh kosong!'];
                 return response($res,422);
             }
@@ -179,13 +150,8 @@ class NasabahController extends Controller
                     $res = ['status' => false, 'message' => 'Nilai Amcoll yang di isi melebihi sisa tunggakan.'];
                     return response($res,422);
                 }
-            } 
+            }
             /** if $sisa_tagihan == 0 , auto lunas **/
-
-            // if(($sisa_tagihan == $json->amcoll)) {
-            //     $res = ['status' => false, 'message' => 'lunas'];
-            //     return response($res,422);
-            // }
             
             if((in_array($json->status_visit, [3,4,5,6,7]))){
         		if($json->catatan == '') {
@@ -205,7 +171,7 @@ class NasabahController extends Controller
                 'tanggal_ptp'   => $json->tanggal_ptp,
                 'catatan'       => $json->catatan,
                 'foto_rumah'    => $foto,
-                'foto_jalan'    => $foto_jalan,
+		        'foto_jalan'    => $foto_jalan,
                 'foto_kwitansi' => $foto_kwitansi,
                 'sisa_tagihan'  => round($sisa_tagihan),  //watch this, important!
                 'lat'           => $json->lat,
--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -1,7 +1,5 @@
 <?php
 namespace App\Http\Controllers\API;
-
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
@@ -82,40 +80,85 @@ class UserController extends Controller
             
             $url = Settings::where('name','=','url')->first();
             $collector = Collector::where('ID','=',$user->id_collection)->first();
-            $nasabah = SignNasabah::select('nasabah.NAMA','nasabah.USER_ID','nasabah.TELEPHONE','nasabah.TELEPHONE','nasabah.ID','mitra.NAMA as MITRA','nasabah.COMPANY','nasabah.KECAMATAN','nasabah.KELURAHAN','nasabah.UPDATED_AT')
+
+            $nasabah = SignNasabah::select('nasabah.NAMA','nasabah.USER_ID','nasabah.TELEPHONE','nasabah.TELEPHONE','nasabah.ID','mitra.NAMA as MITRA','nasabah.COMPANY','nasabah.KECAMATAN','nasabah.KELURAHAN','nasabah.CREATED_AT')
                                   ->join('nasabah','nasabah.ID','=','sign_nasabah.ID_NASABAH')
                                   ->join('mitra','mitra.ID','=','nasabah.ID_MITRA')
                                   ->where('sign_nasabah.IS_AKTIF','=',0)
                                   ->where('nasabah.IS_AKTIF','=',0)
                                   ->where('nasabah.IS_CLOSE','=',0)
+                                  ->where('nasabah.IS_VISIT','=',0)
                                   ->where('sign_nasabah.ID_COLLECTOR','=',$user->id_collection);
+
+            $hasil_visit = SignNasabah::select('nasabah.NAMA','nasabah.USER_ID','nasabah.TELEPHONE','nasabah.TELEPHONE','nasabah.ID','penagihan.STATUS_VISIT as STATUS_VISIT','penagihan.STATUS_BAYAR as STATUS_BAYAR','mitra.NAMA as MITRA','nasabah.COMPANY','nasabah.KECAMATAN','nasabah.KELURAHAN','penagihan.CREATED_AT')
+                                  ->join('nasabah','nasabah.ID','=','sign_nasabah.ID_NASABAH')
+                                  ->join('mitra','mitra.ID','=','nasabah.ID_MITRA')
+                                  ->join('penagihan','penagihan.ID_NASABAH','=','nasabah.ID')
+                                  //->join('nasabah.ID_NASABAH','=','penagihan','ID_NASABAH')
+                                  ->where('sign_nasabah.IS_AKTIF','=',0)
+                                  ->where('nasabah.IS_AKTIF','=',0)
+                                  //->where('nasabah.IS_CLOSE','=',1)
+                                  ->where('nasabah.IS_VISIT','=',1)
+                                  ->where('sign_nasabah.ID_COLLECTOR','=',$user->id_collection)
+                                  ->orderBy('CREATED_AT','DESC');
+
+            $hasil_visit_bayar_full = SignNasabah::select('nasabah.NAMA','nasabah.USER_ID','nasabah.TELEPHONE','nasabah.TELEPHONE','nasabah.ID','penagihan.STATUS_VISIT as STATUS_VISIT','mitra.NAMA as MITRA','nasabah.COMPANY','nasabah.KECAMATAN','nasabah.KELURAHAN','penagihan.CREATED_AT')
+                                  ->join('nasabah','nasabah.ID','=','sign_nasabah.ID_NASABAH')
+                                  ->join('mitra','mitra.ID','=','nasabah.ID_MITRA')
+                                  ->join('penagihan','penagihan.ID_NASABAH','=','nasabah.ID')
+                                  //->join('nasabah.ID_NASABAH','=','penagihan','ID_NASABAH')
+                                  ->where('sign_nasabah.IS_AKTIF','=',0)
+                                  ->where('nasabah.IS_CLOSE','=',0)
+                                  ->where('nasabah.IS_VISIT','=',1)
+                                  ->where('sign_nasabah.ID_COLLECTOR','=',$user->id_collection)
+                                  ->orderBy('CREATED_AT','DESC');
+                                  
 
             $today  = Penagihan::where('ID_COLLECTOR',$user->id_collection)
                         ->whereBetween('CREATED_AT', [date('Y-m-d').' 00:00:00',date('Y-m-d').' 23:59:59']);
+
+                        $jum_visit  = Penagihan::where('ID_COLLECTOR',$user->id_collection);
                       
+            // $total_mitra  = Nasabah::select('nasabah.*')->join('mitra','mitra.ID','=','nasabah.ID_MITRA')->where('sign_nasabah.ID_COLLECTOR','=',$user->id_collection);
 
             if (!empty($keyword)) { // for Search result
                 $result = [];
                 foreach ($nasabah->get() as $cust) {                    
                     if (
                         stripos($cust->NAMA, $keyword) !== false ||
-                        stripos($cust->USER_ID, $keyword) !== false || 
-                        stripos($cust->KECAMATAN, $keyword) !== false || 
-                        stripos($cust->KELURAHAN, $keyword) !== false ) {
+                        stripos($cust->USER_ID, $keyword) !== false ||
+			            stripos($cust->KECAMATAN, $keyword) !== false ||
+			            stripos($cust->KELURAHAN, $keyword) !== false ||
+			            stripos($cust->MITRA, $keyword) !== false  ) {
                         $status = 'found';                        
                         array_push($result, $cust);
                     }
                 }
 
+                $hasil_visit_result = [];
+                    foreach ($hasil_visit->get() as $cust) {
+                        if (
+                            stripos($cust->NAMA, $keyword) !== false ||
+                            stripos($cust->USER_ID, $keyword) !== false ||
+                            stripos($cust->KECAMATAN, $keyword) !== false ||
+                            stripos($cust->KELURAHAN, $keyword) !== false ||
+                            stripos($cust->MITRA, $keyword) !== false  
+                        ) {
+                            $status = 'found';                        
+                            array_push($hasil_visit_result, $cust);
+                        }
+                    }
+
                 if (!empty($result)) {
                     $res = [
                         'success'   => true,
-                        'data'      => $result 
+                        'data'      => $result ,
+                        'data_hasil_visit'      => $hasil_visit_result ,
                     ];
                 } else {
                     $res = [
                         'failed'    => true,
-                        'message'   => 'Data Tidak Di Temukan.'
+                        'message'   => 'Maaf: Data tidak di temukan.'
                     ];
                 }
 
@@ -123,13 +166,20 @@ class UserController extends Controller
             
             } else {  // for display all records related to collectors
                 $res = [
-                    'success'   => true,
-                    'nama'      => $collector->NAMA,
-                    'leader'    => $collector->kordinator->NAMA,
-                    'total'     => $nasabah->count(),
-                    'borrowers' => $today->count(),
-                    'amcoll'    => $today->selectRaw('SUM(AMCOLL) as "AMCOLL"')->pluck('AMCOLL')->first(),
-                    'data'      => $nasabah->get()
+                    'success'               => true,
+                    'nama'                  => $collector->NAMA,
+                    'leader'                => $collector->kordinator->NAMA,
+                    'total'                 => $nasabah->count(),
+                    'total_borrowers'       => $nasabah->count(),
+                    'borrowers'             => $today->count(),
+                    'borrowers_visit'       => $jum_visit->count(),
+                    'amcoll'                => $today->selectRaw('SUM(AMCOLL) as "AMCOLL"')->pluck('AMCOLL')->first(),
+                    'amcoll_visit'          => $jum_visit->selectRaw('SUM(AMCOLL) as "AMCOLL"')->pluck('AMCOLL')->first(),
+                    'data'                  => $nasabah->get(),
+                    'total_borrowers_hasil_visit'  => $hasil_visit->count(),
+                    //'total_mitra'        => $total_mitra->selectRaw('SUM(ID_MITRA) as "ID_MITRA"')->pluck('ID_MITRA')->first(),
+                    // 'status_visit' => $list_penagihan->STATUS_VISIT,
+                    'data_hasil_visit' => $hasil_visit->get()
                 ];
                 return response($res);
             }
@@ -153,12 +203,28 @@ class UserController extends Controller
                               ->where('nasabah.IS_AKTIF','=',0)
                               ->first();
 
+            $nasabah_hasil_visit = Nasabah::select('nasabah.*','mitra.NAMA as MITRA')
+                              ->join('mitra','mitra.ID','=','nasabah.ID_MITRA')
+                              ->where('nasabah.ID','=',$request->get('id_nasabah'))
+                              //->join('nasabah','nasabah.ID','=','penagihan.ID_NASABAH')
+                              //->join('status','status.ID','=','penagihan.STATUS_VISIT')
+                              ->where('IS_CLOSE','=',1)
+                              ->where('nasabah.IS_AKTIF','=',0)
+                              ->first();
+
             $cek_jum = Nasabah::select('nasabah.*','mitra.NAMA as MITRA')
                               ->join('mitra','mitra.ID','=','nasabah.ID_MITRA')
                               ->where('nasabah.ID','=',$request->get('id_nasabah'))
                               ->where('IS_CLOSE','=',0)
                               ->where('nasabah.IS_AKTIF','=',0)
                               ->count();
+
+            // $cek_jum_hasil_visit = Nasabah::select('nasabah.*','mitra.NAMA as MITRA')
+            //                   ->join('mitra','mitra.ID','=','nasabah.ID_MITRA')
+            //                   ->where('nasabah.ID','=',$request->get('id_nasabah'))
+            //                   ->where('IS_CLOSE','=',1)
+            //                   ->where('nasabah.IS_AKTIF','=',0)
+            //                   ->count();
             
             $status = Status::orderBy('NAMA','ASC')->get();
             $bayar = Bayar::orderBy('NAMA','ASC')->get();
@@ -203,7 +269,7 @@ class UserController extends Controller
                         "NAMA"=> $nasabah->NAMA,
                         "TELEPHONE"=> $nasabah->TELEPHONE,
                         "TELEPHONE_RUMAH"=> $nasabah->tanggal_lahir,
-                        "TELEPHONE_KANTOR"=> $nasabah->TELEPHONE_KANTOR,
+                        "TELEPHONE_KANTOR"=> $nasabah->nama_kantor,
                         "ALAMAT"=> $nasabah->ALAMAT_RUMAH,
                         "ALAMAT_KTP"=> $nasabah->alamat_ktp,
                         "ALAMAT_KANTOR"=> $nasabah->ALAMAT_KANTOR,
@@ -218,8 +284,8 @@ class UserController extends Controller
                         "IS_AKTIF"=> $nasabah->IS_AKTIF,
                         "MITRA"=> $nasabah->MITRA,
                         "DPD"=> $nasabah->DPD,
-                        "KECAMATAN"=> $nasabah->KECAMATAN, // sumber database untuk apps
-                        "KELURAHAN"=> $nasabah->KELURAHAN,
+			            "KECAMATAN"=> $nasabah->KECAMATAN,
+			            "KELURAHAN"=> $nasabah->KELURAHAN,
                         "VA_BCA" => $nasabah->VA_BCA,
                         "VA_MANDIRI" => $nasabah->VA_MANDIRI,
                         "VA_PERMATA" => $nasabah->VA_PERMATA,
@@ -228,7 +294,44 @@ class UserController extends Controller
                         "CREATED_AT"=> date('Y-m-d H:i:s',strtotime($nasabah->CREATED_AT)),
                         "UPDATED_AT"=> date('Y-m-d H:i:s',strtotime($nasabah->UPDATED_AT))
                     ];
-            }else{
+            }
+            // else if($cek_jum_hasil_visit > 0){
+            //     $data_nasabah_hasil_visit = [
+            //         "ID"=> $nasabah_hasil_visit->ID,
+            //         "ID_COLLECTOR"=> $user->id_collection,
+            //         "ID_MITRA"=> $nasabah_hasil_visit->ID_MITRA,
+            //         "USER_ID"=>  $nasabah_hasil_visit->USER_ID,
+            //         "NAMA"=> $nasabah_hasil_visit->NAMA,
+            //         "TELEPHONE"=> $nasabah_hasil_visit->TELEPHONE,
+            //         "TELEPHONE_RUMAH"=> $nasabah_hasil_visit->tanggal_lahir,
+            //         "TELEPHONE_KANTOR"=> $nasabah_hasil_visit->nama_kantor,
+            //         "ALAMAT"=> $nasabah_hasil_visit->ALAMAT_RUMAH,
+            //         "ALAMAT_KTP"=> $nasabah_hasil_visit->alamat_ktp,
+            //         "ALAMAT_KANTOR"=> $nasabah_hasil_visit->ALAMAT_KANTOR,
+            //         "TOTAL_TAGIHAN"=> round($nasabah_hasil_visit->TOTAL_TAGIHAN-$tot_amcoll),
+            //         "STATUS_KUNJUNGAN"=>$status_visit,
+            //         "STATUS_PEMBAYARAN" => $status_bayar,
+            //         "CATATAN" => $catatan,
+            //         "FOTO" => $link,
+            //         "AMCOLL" => $amcoll,
+            //         "TANGGAL_PTP" => $tanggal_ptp,
+            //         "IS_CLOSE"=> $nasabah_hasil_visit->IS_CLOSE,
+            //         "IS_AKTIF"=> $nasabah_hasil_visit->IS_AKTIF,
+            //         "MITRA"=> $nasabah_hasil_visit->MITRA,
+            //         "DPD"=> $nasabah_hasil_visit->DPD,
+            //         "KECAMATAN"=> $nasabah_hasil_visit->KECAMATAN,
+            //         "KELURAHAN"=> $nasabah_hasil_visit->KELURAHAN,
+            //         "VA_BCA" => $nasabah_hasil_visit->VA_BCA,
+            //         "VA_MANDIRI" => $nasabah_hasil_visit->VA_MANDIRI,
+            //         "VA_PERMATA" => $nasabah_hasil_visit->VA_PERMATA,
+            //         "CREATED_BY"=> $nasabah_hasil_visit->CREATED_BY,
+            //         "UPDATED_BY"=> $nasabah_hasil_visit->UPDATED_BY,
+            //         "CREATED_AT"=> date('Y-m-d H:i:s',strtotime($nasabah_hasil_visit->CREATED_AT)),
+            //         "UPDATED_AT"=> date('Y-m-d H:i:s',strtotime($nasabah_hasil_visit->UPDATED_AT))
+            //     ];
+            // }
+             else
+            {
                 $data_nasabah = array();
             }
             
@@ -236,6 +339,8 @@ class UserController extends Controller
             $res['bayar'] = $bayar;
             $res['status'] = $status;
             $res['data'] = $data_nasabah;
+            $res['data_hasil_visit'] = $data_nasabah;
+           // $res['data_hasil_visit'] = $data_nasabah_hasil_visit;
             return response($res);
         }catch ( \Exception $e ){
             Log::error($e->getMessage());
