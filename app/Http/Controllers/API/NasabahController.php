c7e915720998c6faf8064cbbdb4d200b4f005268
nauffarms <nauffarms@gmail.com>
2023-03-21 08:07:34 +0700

    update controller

--- app/Http/Controllers/API/NasabahController.php
+++ app/Http/Controllers/API/NasabahController.php
@@ -1,5 +1,7 @@
 <?php 
 namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\DB;
